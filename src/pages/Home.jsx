04ed13d07eeaa2cd9cc4096999643651fd271ca7
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-03-12 06:23:34 +0530

    add on stle branhc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -59,6 +59,8 @@ const Home = () => {
         },
       }
       );
+      setTitle("");
+      setDescription("");
       toast.success(data.message)
       setLoading(false)
       setRefresh(prev=>!prev)
